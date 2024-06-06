7d982bfe4dee26aa78c8a2429488f851841edef6
frndtn <frndtn@alphaphilindo.com>
2024-06-06 17:01:21 +0800

    initial commit

---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+$adb_path='/usr/bin/adb';
+
+$http_authorization='Bearer eyJTZWNyZXRLZXkiOiJKU0JDSVVHRUJESkNCQlNKREc3ODM2RTczVkRWQ0hENzczMyIsIkFwaUtleSI6IkpEQkNKREJWNzYzRTczRDg5SktCSjRCNzc1NjczNUU3SEgifQ==';
+
+$server_url='http://157.245.57.207/deposit';
